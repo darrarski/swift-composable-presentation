d26e65e23b103bafa332f69a44a65c188e94ffe2
Dariusz Rybicki <darrarski@gmail.com>
2021-10-01 14:24:48 +0200

    Add breakpointOnNil parameter to presents modifer

--- Sources/ComposablePresentation/Reducer+PresentsKeyPath.swift
+++ Sources/ComposablePresentation/Reducer+PresentsKeyPath.swift
@@ -7,18 +7,22 @@ extension Reducer {
   ///
   /// - Parameters:
   ///   - localReducer: A reducer that works on `LocalState`, `LocalAction`, `LocalEnvironment`.
+  ///   - breakpointOnNil: If `true`, raises `SIGTRAP` signal when an action is sent to the reducer
+  ///     but state is `nil`. This is generally considered a logic error, as a child reducer cannot
+  ///     process a child action for unavailable child state (defaults to `true`).
   ///   - toLocalState: A key path that can get/set `LocalState` inside `State`.
   ///   - toLocalAction: A case path that can extract/embed `LocalAction` from `Action`.
   ///   - toLocalEnvironment: A function that transforms `Environment` into `LocalEnvironment`.
   /// - Returns: A single, combined reducer.
   public func presents<LocalState, LocalAction, LocalEnvironment>(
     _ localReducer: Reducer<LocalState, LocalAction, LocalEnvironment>,
+    breakpointOnNil: Bool = true,
     state toLocalState: WritableKeyPath<State, LocalState?>,
     action toLocalAction: CasePath<Action, LocalAction>,
     environment toLocalEnvironment: @escaping (Environment) -> LocalEnvironment
   ) -> Self {
     combined(
-      with: localReducer.optional().pullback(
+      with: localReducer.optional(breakpointOnNil: breakpointOnNil).pullback(
         state: toLocalState,
         action: toLocalAction,
         environment: toLocalEnvironment
