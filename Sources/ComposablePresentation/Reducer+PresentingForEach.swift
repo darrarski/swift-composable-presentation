a6bc00c6f5717e067dbf4599c69a73a8e41d130b
Dariusz Rybicki <darrarski@gmail.com>
2021-11-12 16:03:09 +0100

    Add documentation

--- Sources/ComposablePresentation/Reducer+PresentingForEach.swift
+++ Sources/ComposablePresentation/Reducer+PresentingForEach.swift
@@ -1,6 +1,23 @@
 import ComposableArchitecture
 
 extension Reducer {
+  /// Combines the reducer with another reducer that works on elements of `IdentifiedArray`.
+  ///
+  /// - All effects returned by another reducer will be canceled when `LocalState` becomes `nil`.
+  ///
+  /// - Parameters:
+  ///   - localReducer: A reducer that works on `LocalState`, `LocalAction`, `LocalEnvironment`.
+  ///   - breakpointOnNil: If `true`, raises `SIGTRAP` signal when an action is sent to the reducer
+  ///       but state is `nil`. This is generally considered a logic error, as a child reducer cannot
+  ///       process a child action for unavailable child state (defaults to `true`).
+  ///   - toLocalState: A key path from `State` to `IdentifiedArrayOf<LocalState>`.
+  ///   - toLocalAction: A case path that can extract/embed `LocalAction` from `Action`.
+  ///   - toLocalEnvironment: A function that transforms `Environment` into `LocalEnvironment`.
+  ///   - onRun: A closure invoked when another reducer is run. `LocalState.ID` is passed as an argument.
+  ///       Defaults to an empty closure.
+  ///   - onCancel: A closure invoked when effects produced by another reducer are being cancelled.
+  ///       `LocalState.ID` is passed as an argument. Defaults to an empty closure.
+  /// - Returns: A single, combined reducer.
   public func presenting<LocalState, LocalAction, LocalEnvironment>(
     forEach localReducer: Reducer<LocalState, LocalAction, LocalEnvironment>,
     breakpointOnNil: Bool = true,
