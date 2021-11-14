8519cdc4617e8f10694105e912fa7b494bdb6b9e
Dariusz Rybicki <darrarski@gmail.com>
2021-11-14 16:56:36 +0100

    Update iOS example app

---
+++ Example/Example/FullScreenCoverExample.swift
@@ -0,0 +1,111 @@
+import ComposableArchitecture
+import ComposablePresentation
+import SwiftUI
+
+struct FullScreenCoverExample: View {
+  struct MasterState {
+    var detail: DetailState?
+  }
+
+  enum MasterAction {
+    case didTapDetailButton
+    case didDismissDetail
+    case detail(DetailAction)
+  }
+
+  static let masterReducer = Reducer<MasterState, MasterAction, Void> { state, action, _ in
+    switch action {
+    case .didTapDetailButton:
+      state.detail = DetailState()
+      return .none
+
+    case .didDismissDetail:
+      state.detail = nil
+      return .none
+
+    case .detail(_):
+      return .none
+    }
+  }
+    .presenting(
+      detailReducer,
+      state: \.detail,
+      action: /MasterAction.detail,
+      environment: { () }
+    )
+
+  struct MasterView: View {
+    let store: Store<MasterState, MasterAction>
+
+    var body: some View {
+      WithViewStore(store.stateless) { viewStore in
+        Button(action: { viewStore.send(.didTapDetailButton) }) {
+          Text("Detail")
+        }
+        .fullScreenCover(
+          store.scope(
+            state: \.detail,
+            action: MasterAction.detail
+          ),
+          mapState: replayNonNil(),
+          onDismiss: { viewStore.send(.didDismissDetail) },
+          content: DetailView.init(store:)
+        )
+      }
+    }
+  }
+
+  struct DetailState {
+    var timer = TimerState()
+  }
+
+  enum DetailAction {
+    case timer(TimerAction)
+  }
+
+  static let detailReducer = Reducer<DetailState, DetailAction, Void>.combine(
+    timerReducer.pullback(
+      state: \.timer,
+      action: /DetailAction.timer,
+      environment: { () }
+    )
+  )
+
+  struct DetailView: View {
+    let store: Store<DetailState, DetailAction>
+    @Environment(\.presentationMode) var presentationMode
+
+    init(store: Store<DetailState, DetailAction>) {
+      self.store = store
+    }
+
+    var body: some View {
+      VStack {
+        TimerView(store: store.scope(
+          state: \.timer,
+          action: DetailAction.timer
+        ))
+
+        Button(action: { presentationMode.wrappedValue.dismiss() }) {
+          Text("Dismiss").padding()
+        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(Color(.secondarySystemBackground).ignoresSafeArea())
+    }
+  }
+
+  var body: some View {
+    MasterView(store: Store(
+      initialState: MasterState(),
+      reducer: Self.masterReducer,
+      environment: ()
+    ))
+  }
+}
+
+struct FullScreenCoverExample_Previews: PreviewProvider {
+  static var previews: some View {
+    FullScreenCoverExample()
+  }
+}
---
+++ Example/Example/PopToRootExample.swift
@@ -0,0 +1,221 @@
+import ComposableArchitecture
+import ComposablePresentation
+import SwiftUI
+
+struct PopToRootExample: View {
+  struct RootState {
+    var timer = TimerState()
+    var first: FirstState?
+  }
+
+  enum RootAction {
+    case didTapPushFirst
+    case didDismissFirst
+    case timer(TimerAction)
+    case first(FirstAction)
+  }
+
+  static let rootReducer = Reducer<RootState, RootAction, Void>.combine(
+    timerReducer.pullback(
+      state: \.timer,
+      action: /RootAction.timer,
+      environment: { () }
+    ),
+
+    Reducer { state, action, _ in
+      switch action {
+      case .didTapPushFirst:
+        state.first = FirstState()
+        return .none
+
+      case .didDismissFirst:
+        state.first = nil
+        return .none
+
+      case .timer(_):
+        return .none
+
+      case .first(.second(.didTapPopToRoot)):
+        state.first = nil
+        return .none
+
+      case .first(_):
+        return .none
+      }
+    }
+  ).presenting(
+    Self.firstReducer,
+    state: \.first,
+    action: /RootAction.first,
+    environment: { () }
+  )
+
+  struct RootView: View {
+    let store: Store<RootState, RootAction>
+
+    var body: some View {
+      VStack {
+        TimerView(store: store.scope(
+          state: \.timer,
+          action: RootAction.timer
+        ))
+
+        NavigationLinkWithStore(
+          store.scope(
+            state: \.first,
+            action: RootAction.first
+          ),
+          mapState: replayNonNil(),
+          setActive: { active in
+            let viewStore = ViewStore(store.stateless)
+            viewStore.send(active ? .didTapPushFirst : .didDismissFirst)
+          },
+          destination: FirstView.init(store:),
+          label: {
+            Text("Push First").padding()
+          }
+        )
+      }
+      .navigationTitle("Root")
+    }
+  }
+
+  struct FirstState {
+    var timer = TimerState()
+    var second: SecondState?
+  }
+
+  enum FirstAction {
+    case didTapPushSecond
+    case didDismissSecond
+    case timer(TimerAction)
+    case second(SecondAction)
+  }
+
+  static let firstReducer = Reducer<FirstState, FirstAction, Void>.combine(
+    timerReducer.pullback(
+      state: \.timer,
+      action: /FirstAction.timer,
+      environment: { () }
+    ),
+
+    Reducer { state, action, _ in
+      switch action {
+      case .didTapPushSecond:
+        state.second = SecondState()
+        return .none
+
+      case .didDismissSecond:
+        state.second = nil
+        return .none
+
+      case .timer(_):
+        return .none
+
+      case .second(_):
+        return .none
+      }
+    }
+  ).presenting(
+    Self.secondReducer,
+    state: \.second,
+    action: /FirstAction.second,
+    environment: { () }
+  )
+
+  struct FirstView: View {
+    let store: Store<FirstState, FirstAction>
+
+    var body: some View {
+      VStack {
+        TimerView(store: store.scope(
+          state: \.timer,
+          action: FirstAction.timer
+        ))
+
+        NavigationLinkWithStore(
+          store.scope(
+            state: \.second,
+            action: FirstAction.second
+          ),
+          mapState: replayNonNil(),
+          setActive: { active in
+            let viewStore = ViewStore(store.stateless)
+            viewStore.send(active ? .didTapPushSecond : .didDismissSecond)
+          },
+          destination: SecondView.init(store:),
+          label: {
+            Text("Push Second").padding()
+          }
+        )
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(Color(.secondarySystemBackground).ignoresSafeArea())
+      .navigationTitle("First")
+    }
+  }
+
+  struct SecondState {
+    var timer = TimerState()
+  }
+
+  enum SecondAction {
+    case didTapPopToRoot
+    case timer(TimerAction)
+  }
+
+  static let secondReducer = Reducer<SecondState, SecondAction, Void>.combine(
+    timerReducer.pullback(
+      state: \.timer,
+      action: /SecondAction.timer,
+      environment: { () }
+    ),
+
+    Reducer { state, action, _ in
+      switch action {
+      case .didTapPopToRoot:
+        return .none
+
+      case .timer(_):
+        return .none
+      }
+    }
+  )
+
+  struct SecondView: View {
+    let store: Store<SecondState, SecondAction>
+
+    var body: some View {
+      VStack {
+        TimerView(store: store.scope(
+          state: \.timer,
+          action: SecondAction.timer
+        ))
+
+        Button(action: { ViewStore(store.stateless).send(.didTapPopToRoot) }) {
+          Text("Pop to root").padding()
+        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(Color(.tertiarySystemBackground).ignoresSafeArea())
+      .navigationTitle("Second")
+    }
+  }
+
+  var body: some View {
+    NavigationView {
+      RootView(store: Store(
+        initialState: RootState(),
+        reducer: Self.rootReducer,
+        environment: ()
+      ))
+    }
+    .navigationViewStyle(StackNavigationViewStyle())
+  }
+}
+
+struct PopToRootExample_Previews: PreviewProvider {
+  static var previews: some View {
+    PopToRootExample()
+  }
+}
